c2749d4f6906d52fc6743ef74b424d383ae3b901
ycc <772878320@qq.com>
2016-05-30 13:36:23 +0800

    config is ok

---
+++ app/src/main/java/com/yctech/newannott/MainActivity.java
@@ -0,0 +1,10 @@
+package com.yctech.newannott;
+
+import android.app.Activity;
+
+import org.androidannotations.annotations.EActivity;
+
+@EActivity(R.layout.activity_main)
+public class MainActivity extends Activity {
+
+}
